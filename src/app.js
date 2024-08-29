6003bc64c4bfa4353f51535d6e8155a4daccd166
Felipe Xavier <felipexavierass@gmail.com>
2024-08-29 11:47:04 -0300

    include CORS config

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
-require('./routes')
-const { restoreSessions } = require('./sessions')
-const { routes } = require('./routes')
-const app = require('express')()
-const bodyParser = require('body-parser')
-const { maxAttachmentSize } = require('./config')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { restoreSessions } = require('./sessions');
+const { routes } = require('./routes');
+const { maxAttachmentSize } = require('./config');
 
 // Initialize Express app
-app.disable('x-powered-by')
-app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }))
-app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
-app.use('/', routes)
+const app = express();
 
-restoreSessions()
+// CORS configuration
+app.use(cors());
 
-module.exports = app
+// Body parser configuration
+app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }));
+app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }));
+
+// Register routes
+app.use('/', routes);
+
+// Restore sessions
+restoreSessions();
+
+// Disable 'x-powered-by' header
+app.disable('x-powered-by');
+
+module.exports = app;
